43a39d38e9d2885de7c63b12fda1dd32e590d065
AdrianPerera <nikiperera20@gmail.com>
2020-01-10 14:30:51 +0530

    Added fontawsome icons and checked the functionality of desktop notifications

--- static/js/09012020.js
+++ static/js/09012020.js
@@ -7,28 +7,32 @@ function getValuesForModal(){
 }
 
 $("#bell").click(function(){
+//    var tableNo="";
+//    var playerId="";
+//    window.alert(playerId+ " is ready to play at the table "+tableNo);
     window.alert("Player1 is ready to play at the table JNpdG0tUuR63ZLdyMdCe");
  });
 
 
-// Popup window code
+// Popup window
 function newPopup() {
 	popupWindow = window.open("", "MsgWindow", "width=300,height=200");
     popupWindow.document.write("<br><br><hr><p style=\"text-align: center;\">player1 is ready to play at the room 1!</p><hr>" );
 }
 
-//Desktop Notification code
+//Desktop Notification
 function notifyMe() {
+  var initial_message="Hi there! We will notify you on recent player activities on lightning-poker.com";
   if (!("Notification" in window)) {
     alert("This browser does not support desktop notification");
   }
   else if (Notification.permission === "granted") {
-    var notification = new Notification("Hi there!");
+    var notification = new Notification(initial_message);
   }
   else if (Notification.permission !== "denied") {
     Notification.requestPermission().then(function (permission) {
       if (permission === "granted") {
-        var notification = new Notification("Hi there!");
+        var notification = new Notification(initial_message);
       }
     });
   }
