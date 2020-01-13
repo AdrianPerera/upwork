bbc5959c71cb27b017aa59311c8d896b0981beef
AdrianPerera <nikiperera20@gmail.com>
2020-01-13 23:29:20 +0530

    FIX : added new sound and toggle function, refactoring

--- static/js/09012020.js
+++ static/js/09012020.js
@@ -1,3 +1,5 @@
+var notification_permission="default";
+
 function getValuesForModal(){
     var tableNo=1;
     var playerId='anonymous';
@@ -10,28 +12,49 @@ function getValuesForModal(){
 function popup() {
 	popupWindow = window.open("", "MsgWindow", "width=300,height=200");
     popupWindow.document.write("<br><br><hr><p style=\"text-align: center;\">player1 is ready to play at the room 1!</p><hr>" );
-    var audio = new Audio('static/sounds/notify.mp3');
-    audio.play();
+    playAudio();
+}
+
+
+function playAudio(){
+var notification_audio = new Audio('static/sounds/notify.mp3');
+notification_audio.play();
+}
+
+function toggleBell(){
+  $('#bell-slash').toggleClass('hide')
+  $('#bell').toggleClass('hide')
+  $(".subscribe-tab").toggleClass('hide')
 }
 
 //Desktop Notification
 function deviceNotificationSetUp() {
   var initial_message="Hi there! We will notify you on recent player activities on lightning-poker.com";
-  if (!("Notification" in window)) {
-    alert("This browser does not support desktop notification");
-  }
-  else if (Notification.permission === "granted") {
-    var notification = new Notification(initial_message);
-    var audio = new Audio('static/sounds/notify.mp3');
-    audio.play();
-  }
-  else if (Notification.permission !== "denied") {
-    Notification.requestPermission().then(function (permission) {
-      if (permission === "granted") {
-        var notification = new Notification(initial_message);
-      }
-    });
-  }
+  toggleBell();
+  if($('#bell-slash.hide').length===1){
+    var notification_denied=new Notification("We will not send notifications!");
+    var notification_audio = new Audio('static/sounds/denied.mp3');
+    notification_audio.play();
+    //TODO: This needs to be determined later.How to switch of the notifications
+  }else{
+    if (!("Notification" in window)) {
+      alert("This browser does not support desktop notification");
+      var notification_audio = new Audio('static/sounds/denied.mp3');
+      notification_audio.play();
+    }
+    else if (Notification.permission === "granted") {
+      var notification = new Notification("you have already allowed us to send notifications");
+      playAudio();
+    }
+    else if (Notification.permission !== "denied") {
+      Notification.requestPermission().then(function (permission) {
+        if (permission === "granted") {
+          var notification = new Notification(initial_message);
+          playAudio();
+        }
+      });
+    }
 
+  }
 }
 
